3270d5601caef8a9ceb1dda62127c3d5b4dbc08b
JasSainii <jasilpro@gmail.com>
2025-11-03 22:15:23 +0100

    Refactor opera data structure and functions

--- tour.js
+++ tour.js
@@ -1,20 +1,20 @@
 const opere = {
   pittura1: { img: "img/pitture/pitture1.jpg", titolo: "Annunciazione" },
-  pittura2: { img: "/img/pitture/pitture2.jpg", titolo: "Madonna col Bambino" },
-  pittura3: { img: "/img/pitture/pitture3.jpg", titolo: "San Francesco" },
-  pittura4: { img: "/img/pitture/pitture4.jpg", titolo: "Crocifissione" },
-  pittura5: { img: "/img/pitture/pitture5.jpg", titolo: "Ultima Cena" },
-  pittura6: { img: "/img/pitture/pitture6.jpg", titolo: "Resurrezione" },
-  pittura7: { img: "/img/pitture/pitture7.jpg", titolo: "Madonna in trono" },
-  pittura8: { img: "/img/pitture/pitture8.jpg", titolo: "San Giovanni Battista" },
-  quadro1: { img: "/img/quadri/quadro1.jpg", titolo: "Ritratto di Dama" },
-  quadro2: { img: "/img/quadri/quadro2.jpg", titolo: "Paesaggio Montano" },
-  quadro3: { img: "/img/quadri/quadro3.jpg", titolo: "Natura Morta con Frutta" },
-  quadro4: { img: "/img/quadri/quadro4.jpg", titolo: "Veduta di Città" },
-  quadro5: { img: "/img/quadri/quadro5.jpg", titolo: "Ritratto di Gentiluomo" },
-  quadro6: { img: "/img/quadri/quadro6.jpg", titolo: "Marina al Tramonto" },
-  quadro7: { img: "/img/quadri/quadro7.jpg", titolo: "Scena di Battaglia" },
-  quadro8: { img: "/img/quadri/quadro8.jpg", titolo: "Ritratto di Famiglia" }
+  pittura2: { img: "img/pitture/pitture2.jpg", titolo: "Madonna col Bambino" },
+  pittura3: { img: "img/pitture/pitture3.jpg", titolo: "San Francesco" },
+  pittura4: { img: "img/pitture/pitture4.jpg", titolo: "Crocifissione" },
+  pittura5: { img: "img/pitture/pitture5.jpg", titolo: "Ultima Cena" },
+  pittura6: { img: "img/pitture/pitture6.jpg", titolo: "Resurrezione" },
+  pittura7: { img: "img/pitture/pitture7.jpg", titolo: "Madonna in trono" },
+  pittura8: { img: "img/pitture/pitture8.jpg", titolo: "San Giovanni Battista" },
+  quadro1: { img: "img/quadri/quadro1.jpg", titolo: "Ritratto di Dama" },
+  quadro2: { img: "img/quadri/quadro2.jpg", titolo: "Paesaggio Montano" },
+  quadro3: { img: "img/quadri/quadro3.jpg", titolo: "Natura Morta con Frutta" },
+  quadro4: { img: "img/quadri/quadro4.jpg", titolo: "Veduta di Città" },
+  quadro5: { img: "img/quadri/quadro5.jpg", titolo: "Ritratto di Gentiluomo" },
+  quadro6: { img: "img/quadri/quadro6.jpg", titolo: "Marina al Tramonto" },
+  quadro7: { img: "img/quadri/quadro7.jpg", titolo: "Scena di Battaglia" },
+  quadro8: { img: "img/quadri/quadro8.jpg", titolo: "Ritratto di Famiglia" }
 };
 
 // Mostra anteprima
@@ -38,3 +38,4 @@ function nascondiOpera() {
 window.onload = function() {
   imageMapResize();
 };
+
